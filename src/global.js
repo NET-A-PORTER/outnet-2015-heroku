f99bf1cb243b04e475788cf9b82c7a34b5adb83d
Onye Anuna <Onye.Anuna@net-a-porter.com>
2015-04-13 18:04:04 +0100

    Initial commit

---
+++ src/global.js
@@ -0,0 +1,8 @@
+global.base = ***REMOVED***
+	require: (path) => ***REMOVED***
+		return require(this.path(path));
+	***REMOVED***,
+	path: (path) => ***REMOVED***
+		return __dirname + '/' + path;
+	***REMOVED***
+***REMOVED***;
---
+++ src/server.js
@@ -0,0 +1,10 @@
+var app		= require('koa')();
+var router	= require('koa-router');
+var serve	= require('koa-static');
+var utils	= require('./global');
+
+module.exports = app;
+
+require('./core/serve');
+require('./core/router');
+require('./core/start');
