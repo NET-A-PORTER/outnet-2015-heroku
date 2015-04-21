90397b06cdd042df806f0a006a0630d79b069f36
Onye Anuna <Onye.Anuna@net-a-porter.com>
2015-04-21 14:59:10 +0100

    NOJIRA preston styling

--- src/core/serve.js
+++ src/core/serve.js
@@ -1,14 +1,12 @@
 var serve		= require('koa-static');
 var sass		= require('node-sass-middleware');
-var sassDir		= base.path('client/sass');
 var publicDir	= base.path('client');
 var app			= base.require('server');
 
 // compile sass on the fly
 app.use(function * (next) ***REMOVED***
 	yield sass(***REMOVED***
-	    src: base.path('client'),
-	    debug: true
+	    src: publicDir
 	***REMOVED***).bind(sass, this.req, this.res);
 	yield next;
 ***REMOVED***);
