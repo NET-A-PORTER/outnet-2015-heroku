1a14c7d46909448b98a2439179997c235a453351
Onye Anuna <Onye.Anuna@net-a-porter.com>
2015-04-21 17:32:52 +0100

    NOJIRA compile sass from styles directory

--- src/core/serve.js
+++ src/core/serve.js
@@ -1,14 +1,5 @@
 var serve		= require('koa-static');
-var sass		= require('node-sass-middleware');
 var publicDir	= base.path('client');
 var app			= base.require('server');
-
-// compile sass on the fly
-app.use(function * (next) ***REMOVED***
-	yield sass(***REMOVED***
-	    src: publicDir
-	***REMOVED***).bind(sass, this.req, this.res);
-	yield next;
-***REMOVED***);
 
 app.use(serve(publicDir));
