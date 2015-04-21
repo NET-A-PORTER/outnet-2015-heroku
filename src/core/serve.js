b436b6ff40a2759590c7fed5cb122d4fb8585818
Onye Anuna <Onye.Anuna@net-a-porter.com>
2015-04-21 12:27:47 +0100

    NOJIRA compiling sass on the fly

--- src/core/serve.js
+++ src/core/serve.js
@@ -1,5 +1,16 @@
 var serve		= require('koa-static');
-var app			= base.require('server');
+var sass		= require('node-sass-middleware');
+var sassDir		= base.path('client/sass');
 var publicDir	= base.path('client');
+var app			= base.require('server');
+
+// compile sass on the fly
+app.use(function * (next) ***REMOVED***
+	yield sass(***REMOVED***
+	    src: base.path('client'),
+	    debug: true
+	***REMOVED***).bind(sass, this.req, this.res);
+	yield next;
+***REMOVED***);
 
 app.use(serve(publicDir));
