7125a1d1c3a1058428cfef064ce50655b70774c4
Onye Anuna <Onye.Anuna@net-a-porter.com>
2015-04-22 16:56:42 +0100

    NOJIRA style.get controller function

--- src/controllers/styles/index.js
+++ src/controllers/styles/index.js
@@ -1,3 +1,4 @@
+var Style		= require('./style');
 var Styles		= require('./styles');
 var styleDir	= base.path('styles');
 var styles		= null;
@@ -8,13 +9,29 @@ function * list() {
 		styles = styles || new Styles(styleDir);
 		this.body = yield styles.getAll();
 	} catch(e) {
+		console.log(e);
 		this.status = 500;
 		this.body = {
-			message: 'There was an error retrieving styles'
+			message: 'Error retrieving styles'
+		};
+	}
+}
+
+function * get() {
+	try {
+		var name = this.params.name;
+		var style = new Style(name, styleDir);
+		this.body = yield style.getDefinition();
+	} catch(e) {
+		console.log(e);
+		this.status = 500;
+		this.body = {
+			message: 'Error retrieving style ' + name
 		};
 	}
 }
 
 module.exports = {
+	get: get,
 	list: list
 };
