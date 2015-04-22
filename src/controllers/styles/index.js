8512b83da4d2a3b31a3ac86d3cc19b8c5e996bda
Onye Anuna <Onye.Anuna@net-a-porter.com>
2015-04-22 16:56:42 +0100

    NOJIRA style.get controller function

--- src/controllers/styles/index.js
+++ src/controllers/styles/index.js
@@ -1,3 +1,4 @@
+var Style		= require('./style');
 var Styles		= require('./styles');
 var styleDir	= base.path('styles');
 var styles		= null;
@@ -8,13 +9,29 @@ function * list() ***REMOVED***
 		styles = styles || new Styles(styleDir);
 		this.body = yield styles.getAll();
 	***REMOVED*** catch(e) ***REMOVED***
+		console.log(e);
 		this.status = 500;
 		this.body = ***REMOVED***
-			message: 'There was an error retrieving styles'
+			message: 'Error retrieving styles'
+		***REMOVED***;
+	***REMOVED***
+***REMOVED***
+
+function * get() ***REMOVED***
+	try ***REMOVED***
+		var name = this.params.name;
+		var style = new Style(name, styleDir);
+		this.body = yield style.getDefinition();
+	***REMOVED*** catch(e) ***REMOVED***
+		console.log(e);
+		this.status = 500;
+		this.body = ***REMOVED***
+			message: 'Error retrieving style ' + name
 		***REMOVED***;
 	***REMOVED***
 ***REMOVED***
 
 module.exports = ***REMOVED***
+	get: get,
 	list: list
 ***REMOVED***;
--- src/routes/styles.js
+++ src/routes/styles.js
@@ -2,3 +2,4 @@ var app			= base.require('server');
 var controller	= base.require('controllers/styles');
 
 app.get('/api/1.0/styles', controller.list);
+app.get('/api/1.0/styles/:name', controller.get);
