15724c17d8261b7066cc347f0363b0789d200d83
Onye Anuna <Onye.Anuna@net-a-porter.com>
2015-04-24 18:03:42 +0100

    NOJIRA highlightjs

--- src/controllers/styles/content.js
+++ src/controllers/styles/content.js
@@ -1,19 +1,31 @@
-var fs = require('fs');
+var fs	= require('fs');
+var hbs	= require('handlebars');
+
+hbs.registerHelper('markup', (opt) => ***REMOVED***
+	var output = opt.fn();
+	var escaped = hbs.Utils.escapeExpression(output)
+		.replace(/(\r\n|\n|\r)/gm, '<br>')
+		.replace(/(\t)/gm, '&nbsp;&nbsp;');
+	var markup = '<code class="html preston-markup">' + escaped + '</code>';
+	return output + markup;
+***REMOVED***);
 
 function * StyleContent(baseDir) ***REMOVED***
 
 	// read markup in content directory
 	this.markup = yield new Promise((resolve, reject) => ***REMOVED***
 		var options = ***REMOVED*** encoding: 'utf-8', ***REMOVED***;
-		fs.readFile(baseDir + '/markup.html', options, (err, contents) => ***REMOVED***
+		fs.readFile(baseDir + '/markup.hbs', options, (err, contents) => ***REMOVED***
 			if (err) return reject(err);
-			resolve(contents);
+			var template = hbs.compile(contents);
+			var compiled = template();
+			resolve(compiled);
 		***REMOVED***);
 	***REMOVED***);
 	return this;
 ***REMOVED***
 
-StyleContent.prototype.toJSON = function() ***REMOVED***
+StyleContent.prototype.toJSON = () => ***REMOVED***
 	return ***REMOVED***
 		markup: this.markup
 	***REMOVED***;
