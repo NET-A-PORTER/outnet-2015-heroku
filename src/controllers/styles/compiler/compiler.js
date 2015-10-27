ca18b54351e13e98a73b68b5289bb3da36d29302
Onye Anuna <onye.anuna@net-a-porter.com>
2015-10-27 17:54:08 +0000

    NOJIRA fixed random compilation of assets

--- src/controllers/styles/compiler/compiler.js
+++ src/controllers/styles/compiler/compiler.js
@@ -6,7 +6,7 @@ Compiler.prototype = {
 	add: (pattern) => {
 		var tasks = Array.prototype.slice.call(arguments, 1);
 		this.processors.push({
-			pattern: new RegExp(pattern, 'ig'),
+			pattern: pattern,
 			tasks: tasks
 		});
 	},
@@ -20,6 +20,10 @@ Compiler.prototype = {
 			// loop through all processes
 			for (var processIndex in this.processors) {
 				var process = this.processors[processIndex];
+
+				// Regex internal pointer for global pattern must be reset when looped
+				// http://stackoverflow.com/questions/6739136/consecutive-calls-to-regexp-test-fail-for-pattern-with-global-option
+				process.pattern.lastIndex = 0;
 
 				// test if filename matches any process
 				if (process.pattern.test(file)) {
