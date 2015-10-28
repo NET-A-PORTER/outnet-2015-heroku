de679f681bb8f21cb02b21d3268316e2d8f0f844
Onye Anuna <onye.anuna@net-a-porter.com>
2015-10-28 15:00:33 +0000

    NOJIRA yaml for documentation

--- src/controllers/styles/compiler/index.js
+++ src/controllers/styles/compiler/index.js
@@ -1,12 +1,12 @@
-var Compiler		= require('./compiler');
-var HbsCompiler		= require('./compiler.hbs');
+var Compiler			= require('./compiler');
+var YamlCompiler	= require('./compiler.yml');
 var SassCompiler	= require('./compiler.sass');
 var CopyMethod		= require('./method.copy');
-var utils			= base.require('core/utils');
-var assetPath		= base.path('client');
-var compiler		= new Compiler();
+var utils					= base.require('core/utils');
+var assetPath			= base.path('client');
+var compiler			= new Compiler();
 
-compiler.add(/\.hbs/ig, new HbsCompiler());
+compiler.add(/\.yml/ig, new YamlCompiler());
 compiler.add(/(\.woff|\.ttf|\.eot|\.svg|\.png)/ig, new CopyMethod(assetPath));
 compiler.add(/\.scss/ig, new SassCompiler(), function * (directory, file) ***REMOVED***
 	// save to css directory
