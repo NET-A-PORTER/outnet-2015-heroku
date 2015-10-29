3b591175b4311a269d11300d4941458bd328a769
Onye Anuna <onye.anuna@net-a-porter.com>
2015-10-29 16:49:37 +0000

    NOJIRA thank you and goodnight

--- src/controllers/styles/compiler/compiler.doc.js
+++ src/controllers/styles/compiler/compiler.doc.js
@@ -9,16 +9,12 @@ function DocumentCompiler() ***REMOVED***
     var path = directory + '/' + file;
     var result = etc().use(yml).file(path).toJSON();
     return result.document.map((section) => ***REMOVED***
-      var markup = hbs
-          .Utils
-          .escapeExpression(section.markup)
-          .replace(/(\r\n|\n|\r)/gm, '<br>')
-          .replace(/(\t)/gm, '&nbsp;&nbsp;');
+      var markupEscaped = hbs.Utils.escapeExpression(section.markup);
       return ***REMOVED***
         title: section.title,
         detail: section.detail,
         markup: section.markup,
-        markupEscaped: markup
+        markupEscaped: markupEscaped
   ***REMOVED***
 ***REMOVED***);
 ***REMOVED***;
