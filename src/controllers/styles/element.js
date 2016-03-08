c2f6dcd7c1c38e932fe8b4f66757dd6960309e4c
Hem Brahmbhatt <hamant.brahmbhatt@net-a-porter.com>
2016-03-08 11:25:45 +0000

    NOJIRA: moved build to controller

--- src/controllers/styles/element.js
+++ src/controllers/styles/element.js
@@ -2,9 +2,11 @@ var compiler	= require('./compiler');
 var utils 		= base.require('core/utils');
 
 function * StyleElement(opts) ***REMOVED***
-  var pattern = opts.files ? '*(' + opts.files.join('|') + ')' : '**';
-	var assets = yield utils.glob(pattern, ***REMOVED*** cwd: opts.dir ***REMOVED***);
-	this.assets = yield * compiler.process(opts.dir, assets);
+  var files = opts.files;
+  var dir = opts.dir;
+  if(files) files = '*(' + files.join('|') + ')';
+	var assets = yield utils.glob(files || '**', ***REMOVED*** cwd: dir ***REMOVED***);
+	this.assets = yield * compiler.process(dir, assets);
 	return this;
 ***REMOVED***
 
