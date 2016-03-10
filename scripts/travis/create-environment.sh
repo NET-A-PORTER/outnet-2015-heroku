bf2db7a711d8dc2315b7aed6c186be7219b1a5cf
Jerome <jerome.clarke@net-a-porter.com>
2016-03-10 16:27:50 +0000

    printing out message when it should

--- scripts/travis/create-environment.sh
+++ scripts/travis/create-environment.sh
@@ -1,6 +1,5 @@
-echo "Creating test environment"
-
 if [ $TRAVIS_PULL_REQUEST != false ]; then
+    echo "Creating test environment"
     curl -n -X POST https://api.heroku.com/app-setups \
     -H "Content-Type:application/json" \
     -H "Accept:application/vnd.heroku+json; version=3" \
