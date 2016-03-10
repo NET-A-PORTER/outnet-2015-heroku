e4e896d01f752a23a63ee5c17e4c7933e0a969ab
Jerome <jerome.clarke@net-a-porter.com>
2016-03-10 18:05:53 +0000

    better environment script

--- scripts/travis/create-environment.sh
+++ scripts/travis/create-environment.sh
@@ -1,8 +1,61 @@
-if [ $TRAVIS_PULL_REQUEST != false ]; then
-    echo "Creating test environment"
-    curl -n -X POST https://api.heroku.com/app-setups \
-    -H "Content-Type:application/json" \
-    -H "Accept:application/vnd.heroku+json; version=3" \
-    -H "Authorization: Bearer $HEROKU_API_TOKEN" \
-    -d '{"source_blob": { "url":"https://github.com/jay-a-porter/preston/tarball/master"} }'
-fi
+#!/usr/bin/env node
+
+var request = require('request');
+
+var isPullRequest = process.env.TRAVIS_PULL_REQUEST;
+var herokuApiToken = process.env.HEROKU_API_TOKEN;
+var branch = process.env.TRAVIS_BRANCH;
+
+if(isPullRequest) {
+    console.log("Creating test environment")
+    var options = {
+        method: "POST",
+        url: "https://api.heroku.com/app-setups",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/vnd.heroku+json; version=3",
+            "Authorization": "Bearer " + herokuApiToken
+        },
+        body: '{"source_blob": { "url": "https://github.com/jay-a-porter/preston/tarball/' + branch + '"} }'
+    };
+    request(options, function(error, response, body) {
+        console.log('Creating...');
+        if(response.statusCode == 202) {
+            var id = JSON.parse(body).id;
+            var interval = setInterval(function(){
+                appInfo(id, function(info) {
+                    if(info.status != "pending") clearInterval(interval);
+                    if(info.status == "succeeded") {
+                        appInfo(id, function(info) {
+                            console.log(info.resolved_success_url);
+                        });
+                    } else if (info.status != "succeeded" && info.status != "pending") {
+                        console.log('Failed to create envionment. Error:', info.failure_message);
+                    }
+                });
+            }, 5000);
+        } else {
+            console.log('Failed to create envionment. Error:', JSON.parse(body).message);
+        }
+    });
+}
+
+function appInfo(id, callback) {
+
+    var options = {
+        method: "GET",
+        url: "https://api.heroku.com/app-setups/" + id,
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/vnd.heroku+json; version=3",
+            "Authorization": "Bearer " + herokuApiToken
+        }
+    };
+
+    request(options, function(error, response, body) {
+        console.log('polling...');
+        var isPending = JSON.parse(body).status != "succeeded";
+        callback(JSON.parse(body));
+    });
+
+}
