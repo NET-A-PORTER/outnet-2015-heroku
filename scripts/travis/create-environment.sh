e6fc908f39af72c86ef914a92b16a9de59253814
Jerome <jerome.clarke@net-a-porter.com>
2016-03-10 16:21:08 +0000

    creating test environment for pull requests

--- scripts/travis/create-environment.sh
+++ scripts/travis/create-environment.sh
@@ -1,1 +1,9 @@
-echo "Creating environment"
+echo "Creating test environment"
+
+if [ $TRAVIS_PULL_REQUEST != false ]; then
+    curl -n -X POST https://api.heroku.com/app-setups \
+    -H "Content-Type:application/json" \
+    -H "Accept:application/vnd.heroku+json; version=3" \
+    -H "Authorization: Bearer $HEROKU_API_TOKEN" \
+    -d '***REMOVED***"source_blob": ***REMOVED*** "url":"https://github.com/jay-a-porter/preston/tarball/master"***REMOVED*** ***REMOVED***'
+fi
