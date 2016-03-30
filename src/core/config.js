445164882cfc43d689da034314b2817fc3cfabe0
Hem Brahmbhatt <hamant.brahmbhatt@net-a-porter.com>
2016-03-30 16:21:36 +0100

    added default assets and better config management

--- src/core/config.js
+++ src/core/config.js
@@ -2,6 +2,11 @@ var convict = require('convict');
 var schema = base.require('../config/schema.json');
 
 var conf = convict(schema);
-conf.loadFile('config/' + conf.get('env') + '.json');
+
+try {
+  conf.loadFile('config/' + conf.get('env') + '.json');
+} catch (e) {
+  console.error('Couldn\'t load config for', conf.get('env'));
+}
 
 module.exports = conf;
