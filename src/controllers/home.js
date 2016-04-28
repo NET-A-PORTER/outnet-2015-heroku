671fcf242073b460e6262d65dc3c4aa14f72b769
Hem Brahmbhatt <hamant.brahmbhatt@net-a-porter.com>
2016-04-28 14:21:40 +0100

    compiling styles on page refresh locally

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,4 +1,9 @@
+var config = base.require('core/config');
+var build = require('./styles').build;
+
 function * render() {
+  if(config.get('env') == 'local')
+    yield * build();
 	yield this.render('home');
 }
 
