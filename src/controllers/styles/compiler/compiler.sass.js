8b54fa5e81065cd0ee9204af0d1f517ba5f3cfe1
Hem Brahmbhatt <hamant.brahmbhatt@net-a-porter.com>
2016-04-28 15:35:22 +0100

    can compile nested scss files

--- src/controllers/styles/compiler/compiler.sass.js
+++ src/controllers/styles/compiler/compiler.sass.js
@@ -5,10 +5,13 @@ function SassCompiler() {
 	return function * (directory, file) {
 		// async sass.render isn't working for some reason
 		var path = directory + '/' + file;
+    var includePaths = path.split('/');
+    includePaths.pop();
+    includePaths = includePaths.join('/');
 		var source = yield utils.readFile(path);
 		var compiled = sass.renderSync({
 			data: source,
-			includePaths: [directory]
+			includePaths: [includePaths]
 		}).css;
 
 		return {
